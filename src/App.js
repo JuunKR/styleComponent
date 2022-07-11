873f86ba2904cbbda068f204b4ecea40130d0f7f
JuunKR <bluejun0510@naver.com>
2022-07-11 22:04:16 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled, { keyframes, Keyframes } from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vw;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const BigFather = styled.div``;
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+const Btn = styled.button`
+  color: white;
+  background-color: teal;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
+const animation = keyframes`
+  0%{
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50%{
+    transform:rotate(360deg);
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+`;
+const Emoji = styled.span`
+  font-size: 30px;
+`;
+
+const AnimaBox = styled(Box)`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  animation: ${animation} 1s linear infinite;
+  span {
+    font-size: 55px;
+    &:hover {
+      font-size: 88px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+  ${Emoji}:hover {
+    font-size: 88px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <BigFather>
+        <Title>Hello</Title>
+
+        <Father as="header">
+          <Box bgColor="teal">
+            <Text>Hello</Text>
+          </Box>
+          <Circle bgColor="tomato">
+            <Btn as="a" href="/">
+              Click Me
+            </Btn>
+          </Circle>
+        </Father>
+        <Father>
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+        </Father>
+        <Father>
+          <AnimaBox>
+            <span>😊</span>
+            <Emoji as="div">💕</Emoji>
+          </AnimaBox>
+          <Emoji as="div">😁</Emoji>
+        </Father>
+      </BigFather>
+    </Wrapper>
   );
 }
 
